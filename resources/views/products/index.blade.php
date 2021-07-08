7a6740c69d631218111569daee22002ebfadbcde
“Jeyson <jjrodriguezo@indracompany.com>
2021-07-08 10:41:48 -0500

    Subida del Proyecto - first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lists of products</title>
+</head>
+<body>
+    <h1>List of products</h1>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead class="thead-light">
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Soap</td>
+                    <td>Best Soap</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Soap</td>
+                    <td>Best soap ever</td>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</body>
+</html>
