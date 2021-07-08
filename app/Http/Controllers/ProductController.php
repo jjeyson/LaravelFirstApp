779bfbee938e1884b02bde0bd7c114bc8e7ec078
“Jeyson <jjrodriguezo@indracompany.com>
2021-07-08 12:19:21 -0500

    Add factory Product and working with query Builder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        $products = DB::table('products')->get();
+        dd($products);
         return view('products.index');
     }
     public function create(){
@@ -15,6 +18,9 @@ class ProductController extends Controller
         //
     }
     public function show($product){
+        // $product = DB::table('products')->where('id',$product)->first();
+        $product = DB::table('products')->find($product);
+        dd($product);
         return view('products.show');
     }
     public function edit($product){
