d9420ad52c5b67bb8c46080187c2ebf480d47370
“Jeyson <jjrodriguezo@indracompany.com>
2021-07-12 17:50:10 -0500

    new migrate also modify in new models

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'amount',
+        'payed_at',
+    ];
+
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [];
 }
